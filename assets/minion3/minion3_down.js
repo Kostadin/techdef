a86e13cf648f6202f9c3785f6d278a36fffb377a
dbatashki <dbatashki@abv.bg>
2017-01-21 17:04:03 +0200

    minions2 and 3

--- assets/minion3/minion3_down.js
+++ assets/minion3/minion3_down.js
@@ -1,12 +1,12 @@
 {
 "frames": {
 	"minion3_down1.png": {
-		"frame": {"x":26, "y":0, "w":25, "h":27},
-		"spriteSourceSize": {"x":3,"y":3,"w":32,"h":32},
+		"frame": {"x":25, "y":0, "w":24, "h":27},
+		"spriteSourceSize": {"x":4,"y":3,"w":32,"h":32},
 		"sourceSize": {"w":32,"h":32}
 	},
 	"minion3_down2.png": {
-		"frame": {"x":0, "y":0, "w":25, "h":27},
+		"frame": {"x":0, "y":0, "w":24, "h":27},
 		"spriteSourceSize": {"x":4,"y":3,"w":32,"h":32},
 		"sourceSize": {"w":32,"h":32}
 	}
@@ -14,7 +14,7 @@
 },
 "meta": {
 	"image": "minion3_down.png",
-	"size": {"w": 52, "h": 28},
+	"size": {"w": 50, "h": 28},
 	"scale": "1"
 }
 }
