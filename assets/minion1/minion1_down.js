293aab5bf503e2a5ef2791fdfd227b6004a75fb0
dbatashki <dbatashki@abv.bg>
2017-01-21 13:43:36 +0200

    minion and tower 1

---
+++ assets/minion1/minion1_down.js
@@ -0,0 +1,20 @@
+{
+"frames": {
+	"minion1left.png": {
+		"frame": {"x":22, "y":0, "w":21, "h":29},
+		"spriteSourceSize": {"x":5,"y":2,"w":32,"h":32},
+		"sourceSize": {"w":32,"h":32}
+	},
+	"minion1right.png": {
+		"frame": {"x":0, "y":0, "w":21, "h":29},
+		"spriteSourceSize": {"x":6,"y":2,"w":32,"h":32},
+		"sourceSize": {"w":32,"h":32}
+	}
+
+},
+"meta": {
+	"image": "minion1_down.png",
+	"size": {"w": 44, "h": 30},
+	"scale": "1"
+}
+}
