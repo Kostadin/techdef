9bac52d36bf5ec236a7d4f17fbbb70902cd53d98
dbatashki <dbatashki@abv.bg>
2017-01-21 17:50:34 +0200

    minion boss

---
+++ assets/minion_boss/minionboss_down.js
@@ -0,0 +1,20 @@
+{
+"frames": {
+	"minionboss_down1.png": {
+		"frame": {"x":47, "y":0, "w":46, "h":56},
+		"spriteSourceSize": {"x":7,"y":2,"w":60,"h":60},
+		"sourceSize": {"w":60,"h":60}
+	},
+	"minionboss_down2.png": {
+		"frame": {"x":0, "y":0, "w":46, "h":56},
+		"spriteSourceSize": {"x":7,"y":2,"w":60,"h":60},
+		"sourceSize": {"w":60,"h":60}
+	}
+
+},
+"meta": {
+	"image": "minionboss_down.png",
+	"size": {"w": 94, "h": 57},
+	"scale": "1"
+}
+}
