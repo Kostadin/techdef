7c2d6ee6fe6418d738d08e83b9280e30dd974e63
dbatashki <dbatashki@abv.bg>
2017-01-21 15:26:48 +0200

    minion2

---
+++ assets/minion2/minion2_down.js
@@ -0,0 +1,20 @@
+{
+"frames": {
+	"minion2_down1.png": {
+		"frame": {"x":25, "y":0, "w":24, "h":30},
+		"spriteSourceSize": {"x":3,"y":1,"w":32,"h":32},
+		"sourceSize": {"w":32,"h":32}
+	},
+	"minion2_down2.png": {
+		"frame": {"x":0, "y":0, "w":24, "h":30},
+		"spriteSourceSize": {"x":5,"y":1,"w":32,"h":32},
+		"sourceSize": {"w":32,"h":32}
+	}
+
+},
+"meta": {
+	"image": "minion2_down.png",
+	"size": {"w": 50, "h": 31},
+	"scale": "1"
+}
+}
