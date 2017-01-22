5589a52612855e76ed8bb75110788257814ad46f
Kostadin Vodenicharov <kostadinvo@gmail.com>
2017-01-22 14:41:18 +0200

    Level 0 changes

--- levels/level0.js
+++ levels/level0.js
@@ -246,7 +246,7 @@ levels[0] =
 	],
 	"definition" : 
 	{
-		"w" : { "type" : "wall", "sprite" : "wall1.png" , "passable" : false, "buildable" : true },
+		"w" : { "type" : "wall", "sprite" : "wall1.png" , "passable" : false, "buildable" : false },
 		"o" : { "type" : "floor", "sprite" : "ground1.png" , "passable" : true, "buildable" : false },
 		"i" : 
 		{
