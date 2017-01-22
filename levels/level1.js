173709a58745cc44e768fd2a5600a86f7d9ee272
dbatashki <dbatashki@abv.bg>
2017-01-22 14:25:23 +0200

    level1

---
+++ levels/level1.js
@@ -0,0 +1,301 @@
+levels[1] = 
+{	
+	"map" :
+	[
+		[ "w", "w", "w", ["o", "i"], "w", "w", "w", "w", "w", ["o", "i"], ["o", "i"], "w", "w", ["o", "i"], "w", "w", "w", "w", "w", "w"],
+		[ "w", "w", "w", "o", "w", "w", "w", "w", "w", "o", "o", "w", "w", "o", "w", "w", "w", "w", "w", "w"],
+		[ "w", "o", "o", "o", "w", "w", "w", "w", "o", "o", "o", "o", "w", "o", "o", "o", "o", "o", "o", "o"],
+		[ "o", "o", "o", "o", "w", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
+		[ "o", "w", "w", ["w", "g"], "o", "o", "w", "w", "o", "o", "o", "w", "o", "o", "w", "w", ["w", "g"], "w", "w", "o"],
+		[ "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o"],
+		[ "w", "o", "w", "w", "w", ["w", "g"], "o", "o", "o", "o", "o", "o", "o", "o", "w", "w", "w", "w", "o", "w"],
+		[ "w", "o", "w", "w", "w", "w", "w", "o", "o", ["w", "g"], "o", "o", "o", "w", ["w", "g"], "w", "w", "w", "o", "w"],
+		[ "w", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "w"],
+		[ "w", "w", ["w", "g"], "w", "w", "w", "w", "w", "w", "o", "o", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
+		[ "w", "w", "w", "w", "w", "w", "w", "w", "w", ["o", "e"], ["o", "e"], "w", "w", "w", "w", "w", "w", "w", "w", "w"]
+	],
+	"waves" : [
+		{
+			"startMS": 1000,
+			"count": 60,
+			"interval": 100,
+			"x": 3,
+			"y": 0,
+			"speed": 6,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion2"
+		},
+		{
+			"startMS": 2000,
+			"count": 60,
+			"interval": 100,
+			"x": 13,
+			"y": 0,
+			"speed": 6,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion1"
+		},
+		{
+			"startMS": 3000,
+			"count": 5,
+			"interval": 100,
+			"x": 9,
+			"y": 0,
+			"speed": 6,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion2"
+		},
+		{
+			"startMS": 6000,
+			"count": 15,
+			"interval": 100,
+			"x": 3,
+			"y": 0,
+			"speed": 6,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion1"
+		},
+		{
+			"startMS": 7000,
+			"count": 8,
+			"interval": 100,
+			"x": 13,
+			"y": 0,
+			"speed": 6,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion2"
+		},
+		{
+			"startMS": 8000,
+			"count": 6,
+			"interval": 100,
+			"x": 9,
+			"y": 0,
+			"speed": 4,
+			"maxHP": 200,
+			"sizeX": 58,
+			"sizeY": 58,
+			"sheet": "minionboss"
+		},
+		{
+			"startMS": 9000,
+			"count": 60,
+			"interval": 150,
+			"x": 3,
+			"y": 0,
+			"speed": 6,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion1"
+		},
+		{
+			"startMS": 10000,
+			"count": 12,
+			"interval": 150,
+			"x": 13,
+			"y": 0,
+			"speed": 6,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion1"
+		},
+		{
+			"startMS": 11000,
+			"count": 60,
+			"interval": 150,
+			"x": 9,
+			"y": 0,
+			"speed": 4,
+			"maxHP": 200,
+			"sizeX": 58,
+			"sizeY": 58,
+			"sheet": "minionboss"
+		},
+		{
+			"startMS": 20000,
+			"count": 9,
+			"interval": 150,
+			"x": 3,
+			"y": 0,
+			"speed": 8,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion2"
+		},
+		{
+			"startMS": 21000,
+			"count": 12,
+			"interval": 150,
+			"x": 9,
+			"y": 0,
+			"speed": 8,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion2"
+		},
+		{
+			"startMS": 24000,
+			"count": 11,
+			"interval": 150,
+			"x": 13,
+			"y": 0,
+			"speed": 8,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion1"
+		},
+		{
+			"startMS": 26000,
+			"count": 6,
+			"interval": 150,
+			"x": 9,
+			"y": 0,
+			"speed": 4,
+			"maxHP": 100,
+			"sizeX": 58,
+			"sizeY": 58,
+			"sheet": "minionboss"
+		},
+		{
+			"startMS": 30000,
+			"count": 12,
+			"interval": 150,
+			"x": 13,
+			"y": 0,
+			"speed": 8,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion2"
+		},
+		{
+			"startMS": 31000,
+			"count": 9,
+			"interval": 150,
+			"x": 3,
+			"y": 0,
+			"speed": 8,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion1"
+		},
+		{
+			"startMS": 33000,
+			"count": 6,
+			"interval": 150,
+			"x": 9,
+			"y": 0,
+			"speed": 6,
+			"maxHP": 100,
+			"sizeX": 58,
+			"sizeY": 58,
+			"sheet": "minionboss"
+		},
+		{
+			"startMS": 38000,
+			"count": 60,
+			"interval": 150,
+			"x": 3,
+			"y": 0,
+			"speed": 8,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion1"
+		},
+		{
+			"startMS": 38000,
+			"count": 12,
+			"interval": 150,
+			"x": 13,
+			"y": 0,
+			"speed": 8,
+			"maxHP": 100,
+			"sizeX": 32,
+			"sizeY": 32,
+			"sheet": "minion2"
+		},
+		{
+			"startMS": 39000,
+			"count": 5,
+			"interval": 150,
+			"x": 13,
+			"y": 0,
+			"speed": 8,
+			"maxHP": 100,
+			"sizeX": 58,
+			"sizeY": 58,
+			"sheet": "minionboss"
+		}
+	],
+	"definition" : 
+	{
+		"w" : { "type" : "wall", "sprite" : "wall1.png" , "passable" : false, "buildable" : true },
+		"o" : { "type" : "floor", "sprite" : "ground1.png" , "passable" : true, "buildable" : true },
+		"i" : 
+		{
+			"type" : "entrance",
+			"sprite" : "entrance.png",
+			"passable" : true,
+			"buildable" : false
+		},
+		"g" : {
+			"type" : "towerground",
+			"sprite" : "tower_ground.png",
+			"passable" : false,
+			"buildable" :true
+		},
+		"e" : 
+		{
+			"type" : "exit",
+			"sprite" : "exit1.png",
+			"passable" : true,
+			"buildable" : false
+		}
+	},
+	"tower_templates" : {
+		"q" : {
+			"projectile": "plasma",
+			"interval": 100,
+			"duration": 50,
+			"range": 220,
+			"damage": 20,
+			"aoe": 0,
+			"sheet": "tower1"
+		},
+		"w" : {
+			"projectile": "laser",
+			"interval": 330,
+			"duration":250,
+			"range": 350,
+			"damage": 20,
+			"aoe": 10,
+			"sheet": "tower3"
+		},
+		"e" : {
+			"projectile": "granade",
+			"interval": 500,
+			"duration": 50,
+			"range": 220,
+			"damage": 40,
+			"aoe": 32,
+			"sheet": "tower2"
+		}
+	}
+};
